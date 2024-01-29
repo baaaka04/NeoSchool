3ea6f47bd355021c721ff74a5a36cc8f862c3092
baaaka04 <baaaka04@yahoo.com>
2024-01-29 19:25:16 +0600

    Initial Commit

---
+++ NeoSchool/MainTabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NeoSchool
+//
+//  Created by Artem Berezin on 29/1/24.
+//
+
+import UIKit
+
+class MainTabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
